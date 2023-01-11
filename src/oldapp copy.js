25d4964b85c6d6e466da2712ee558fb659a1250a
ynavach <yunensey@hotmail.com>
2023-01-11 15:20:28 +0000

    Mi primer commit

---
+++ src/oldapp copy.js
@@ -0,0 +1,16 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let x = 0;
+do {
+  let i = 0;
+  while (i <= adj.length) {
+    for (let j = 0; j < noun.length; j++) {
+      const new_domain = pronoun[x] + adj[i] + noun[j] + ".com";
+    console.log(new_domain);
+    }
+    i++;
+  }
+  x++;
+} while (x <= pronoun.length);
